d5adf9f993806ba06ddd93b28ccb0822186ff5e0
ycl <455166539@qq.com>
2020-07-31 10:50:39 +0800

    优化路由方法，更直观，修改发布脚本

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # lono-router
-lono-router
-
 TODO
-// @Router
-// @Header 请求头部 序列化
-// @Get
-// @Post
-// @Put
-// @Del
-// @Patch
-// @Auth 权限 promise 指向 cookie session jwt midd(404 401 auth)
-// 路由path正则
-// multer支持文件上传
+- [ ] 增加@Header 优化设置请求头部和序列化
+- [ ] 增加@Auth 权限支持传参一个promise 指向 cookie session jwt midd(404 401 auth)
+- [ ] 路由增加file参数，支持文件上传
+- [ ] 路由path优化支持正则表达式
--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,50 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.route = route;
 exports.default = _default;
+Object.defineProperty(exports, "Controller", {
+  enumerable: true,
+  get: function () {
+    return _decorator.Controller;
+  }
+});
+Object.defineProperty(exports, "Router", {
+  enumerable: true,
+  get: function () {
+    return _decorator.Router;
+  }
+});
+Object.defineProperty(exports, "Get", {
+  enumerable: true,
+  get: function () {
+    return _decorator.Get;
+  }
+});
+Object.defineProperty(exports, "Post", {
+  enumerable: true,
+  get: function () {
+    return _decorator.Post;
+  }
+});
+Object.defineProperty(exports, "Put", {
+  enumerable: true,
+  get: function () {
+    return _decorator.Put;
+  }
+});
+Object.defineProperty(exports, "Del", {
+  enumerable: true,
+  get: function () {
+    return _decorator.Del;
+  }
+});
+Object.defineProperty(exports, "Patch", {
+  enumerable: true,
+  get: function () {
+    return _decorator.Patch;
+  }
+});
 exports.HttpMethod = void 0;
-
-var utils = _interopRequireWildcard(require("./utils"));
 
 var _router = _interopRequireDefault(require("@koa/router"));
 
@@ -23,11 +62,9 @@ var _glob = _interopRequireDefault(require("glob"));
 
 var _fs = _interopRequireDefault(require("fs"));
 
+var _decorator = require("./decorator.js");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
@@ -35,36 +72,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
 const cwd = process.cwd();
 const upload = (0, _multer.default)();
-const CONTEXT_SERVICE = Symbol('context#service'); // const bindFn = (fn, ctx) => {
-//   const data = {}
-//   if (typeof fn == 'function') {
-//     return fn.bind(ctx)
-//   } else if (typeof fn == 'object') {
-//     for (const i in fn) {
-//       data[i] = bindFn(fn[i], ctx)
-//     }
-//   }
-//   return data
-// }
-
-const setCtx = (obj, ctx) => {
-  for (let k in obj) {
-    obj[k].ctx = ctx;
-  }
-};
-
-const serviceMap = {};
-const serviceSource = {}; // @Router
-// @Header 请求头部 序列化
-// @Get
-// @Post
-// @Put
-// @Del
-// @Patch
-// @Auth 权限 promise 指向 cookie session jwt midd(404 401 auth)
-// 路由path正则
-// multer支持文件上传
-
+const CONTEXT_SERVICE = Symbol('context#service');
 const HttpMethod = {
   ALL: 'all',
   HEAD: 'head',
@@ -76,95 +84,20 @@ const HttpMethod = {
   DELETE: 'delete',
   DEL: 'del'
 };
-/**
- * 一个路由单元
- */
-
 exports.HttpMethod = HttpMethod;
+const serviceMap = {};
+const serviceSource = {};
 
-class Route {
-  constructor(opt = {}) {
-    this.path = '';
-    this.middleware = [];
-    this.prefix = null;
-    this.router = [];
+const setCtx = (obj, ctx) => {
+  for (let k in obj) {
+    obj[k].ctx = ctx;
   }
+};
 
-} // route({multipart: 'fields', data: []    multipart: 'single', data: 'aaa'})
-
-
-function route(path, method, middleware = []) {
-  if (typeof path === 'object') {
-    const params = { ...path
-    };
-    method = params.method;
-    middleware = params.middleware || [];
-    path = params.path; // if (params.multipart) {
-    //   switch (params.multipart) {
-    //     case 'fields':
-    //       middleware.push(upload.fields(params.data))
-    //       break
-    //     case 'single':
-    //       middleware.push(upload.single(params.data))
-    //       break
-    //   }
-    // }
-  }
-
-  return function (target, key, descriptor) {
-    utils.assert(!!path, '@route should have at least "path" argument'); // 兼容静态方法和一般方法
-
-    const C = target.prototype ? target.prototype : target;
-
-    if (!C.route) {
-      C.route = new Route();
-    }
-    /**
-     * 修饰类
-     */
-
-
-    if (typeof target === 'function' && key === undefined && descriptor === undefined) {
-      utils.assert(!method, '@route on Class should not have "method"');
-      target.prototype.route.prefix = path;
-
-      if (middleware.length > 0) {
-        target.prototype.route.middleware = target.prototype.route.middleware.concat(middleware);
-      }
-
-      return;
-    }
-    /**
-     * 修饰方法
-     */
-
-
-    !method && (method = 'all');
-    utils.assert(!!method, '@route on method should have "method" as second argument'); // TODO 修改bind this 指向ctx 或者lode
-
-    C.route.router.push({
-      path: path,
-      method: method,
-      middleware: middleware,
-      callback: function () {
-        var _ref = _asyncToGenerator(function* (ctx, next) {
-          const result = yield descriptor.value(ctx, next);
-          ctx.body = ctx.body || result;
-        });
-
-        return function callback(_x, _x2) {
-          return _ref.apply(this, arguments);
-        };
-      }()
-    });
-  };
-} // TODO 优化route参数支持对象
-
-
-class Router {
+class LRouter {
   constructor(opt = {}) {
     this.name = 'router';
-    this.isLode = true;
+    this.isLono = true;
     this.routes = [];
     this.opt = {
       path: opt.path,
@@ -185,7 +118,7 @@ class Router {
     this.router = new _router.default(opt);
   }
 
-  install(lode) {
+  install(app) {
     // 加载路由文件
     (0, _require.default)(this.opt);
     (0, _require.default)({
@@ -194,12 +127,12 @@ class Router {
       onAfter: (Service, file) => {
         if (!Service.default) return;
         const c = new Service.default({
-          app: lode,
+          app: app,
           service: serviceMap,
           ctx: null,
-          config: lode.$config,
-          curl: lode.$curl,
-          log: lode.$logger
+          config: app.$config,
+          curl: app.$curl,
+          log: app.$logger
         });
         serviceSource[file.prefix] = c;
         const path = `${file.dirs && file.dirs.length ? file.dirs.join('.') + '.' : ''}${file.prefix}`;
@@ -207,21 +140,21 @@ class Router {
         _lodash.default.set(serviceMap, path, c);
       }
     });
-    Object.defineProperty(lode.context, 'service', {
+    Object.defineProperty(app.context, 'service', {
       value: serviceMap,
       writable: false
     });
-    lode.use( /*#__PURE__*/function () {
-      var _ref2 = _asyncToGenerator(function* (ctx, next) {
+    app.use( /*#__PURE__*/function () {
+      var _ref = _asyncToGenerator(function* (ctx, next) {
         setCtx(serviceSource, ctx);
         yield next();
       });
 
-      return function (_x3, _x4) {
-        return _ref2.apply(this, arguments);
+      return function (_x, _x2) {
+        return _ref.apply(this, arguments);
       };
     }());
-    lode.use(this.router.routes()).use(this.router.allowedMethods());
+    app.use(this.router.routes()).use(this.router.allowedMethods());
   }
 
   createKoaRouter(route) {
@@ -236,6 +169,7 @@ class Router {
     }
 
     route.router.forEach((v, i) => {
+      // TODO 考虑callback的this指向需不需要优化
       router[v.method](v.path, ...v.middleware, v.callback);
     });
     return router;
@@ -244,5 +178,5 @@ class Router {
 }
 
 function _default(...arg) {
-  return new Router(...arg);
+  return new LRouter(...arg);
 }
